3a601e7f40411d282e1744edc658893cc34ef46f
20063209 <20063209@it103imac08.wit.ie>
2017-04-05 20:30:21 +0100

    Initial Commit

---
+++ PokeStep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeStep
+//
+//  Created by 20063209 on 05/04/2017.
+//  Copyright © 2017 WIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
