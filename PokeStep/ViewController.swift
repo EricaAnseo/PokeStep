76d792b05e71d9569bdbde76e67aa3896f1574f4
20063209 <20063209@it103imac08.wit.ie>
2017-05-04 10:41:11 +0100

    Added App Icon and can read from Json file

--- PokeStep/ViewController.swift
+++ PokeStep/ViewController.swift
@@ -13,11 +13,70 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        readJson()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func readJson() {
+        do {
+            if let file = Bundle.main.url(forResource: "pokedex", withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String: Any] {
+                    // json is a dictionary
+                    print(object)
+                } else if let object = json as? [Any] {
+                    // json is an array
+                    print(object)
+                } else {
+                    print("JSON is invalid")
+                }
+            } else {
+                print("no file")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func callPokemonByNumber()
+    {
+        do{
+            //updated by xcode, if the json file is not null
+            if let file = Bundle.main.url(forResource: "pokedex", withExtension: "json")  {
+                let data = try Data(contentsOf: file)
+                _ = try JSONSerialization.jsonObject(with: data, options: [])
+                /*if let dictionary = jsonWithObjectRoot as? [String: Any] {
+                    if let number = dictionary["someKey"] as? Double {
+                        // access individual value in dictionary
+                    }
+                    
+                    for (key, value) in dictionary {
+                        // access all key / value pairs in dictionary
+                    }
+                    
+                    if let nestedDictionary = dictionary["anotherKey"] as? [String: Any] {
+                        // access nested dictionary values by key
+                    }
+                }*/
+                
+                
+            }
+
+        }
+            
+        //handles any errors that may occur from reading the json file
+        catch {
+            print(error.localizedDescription)
+        }
+        
     }
 
 
