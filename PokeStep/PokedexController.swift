efa2682619f21bf36292cc246b08eacea2692f1f
20063209 <20063209@it103imac08.wit.ie>
2017-05-22 16:05:31 +0100

    Updated Storyboard, Collection View, and loading images online

--- PokeStep/PokedexController.swift
+++ PokeStep/PokedexController.swift
@@ -15,7 +15,7 @@ class PokedexCollectionViewController: UICollectionViewController {
     
     var pokedex = [[String:Any]]()
     var names = [String]()
-    var pokemonImages = [String]()
+    var pokemonImages = [UIImage]()
     
 
     override func viewDidLoad() {
@@ -38,26 +38,13 @@ class PokedexCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let pokemon = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemon", for: indexPath) as UICollectionViewCell
 
-        readPokemonJson()
-        
-        //let pokedex:[String: Any] =
-        
-        //let pocketMonsters = pokedex["pokemon"] as? [[String: AnyObject]]
-        
-        //for pokemon in pocketMonsters! {
-            //let num = pokemon["num"] as? String
-            
-
-        //}
-        
-        
         //Tagged assigned in storyboard. Tag 1 is assigned to the UIimage
-        //let pokemonImage = pokemon.viewWithTag(1) as! UIImageView
+        let pokemonImage = pokemon.viewWithTag(1) as! UIImageView
         
         //Tag 2 is assigned to the UILabel
         let pokemonName = pokemon.viewWithTag(2) as! UILabel
         
-        //pokemonImage.image = pokemonImages[indexPath.row]
+        pokemonImage.image = pokemonImages[indexPath.row]
 
         pokemonName.text = names[indexPath.row]
         
@@ -80,12 +67,50 @@ class PokedexCollectionViewController: UICollectionViewController {
                         if let name = pokemon["name"] as? String {
                             names.append(name)
                         }else { break }
-                        if let image = pokemon["img"] as? String {
-                            pokemonImages.append(image)
-                        }else { break }
-                    }
+                        if let imageURL = pokemon["img"] as? String {
+                            
+                            //dispatch_async(DispatchQueue.global( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                                
+                                let myImage = try UIImage(data: NSData(contentsOf: NSURL(string:imageURL) as! URL) as Data)
+                                self.pokemonImages.append(myImage!)
+                           // })
+                            
+                            /*let pokemonPictureURL = URL(string: imageURL)!
+                            
+                            // Creating a session object with the default configuration.
+                            // You can read more about it here https://developer.apple.com/reference/foundation/urlsessionconfiguration
+                            let session = URLSession(configuration: .default)
+                            
+                            // Define a download task. The download task will download the contents of the URL as a Data object and then you can do what you wish with that data.
+                            let downloadPicTask = session.dataTask(with: pokemonPictureURL) { (data, response, error) in
+                                // The download has finished.
+                                if let e = error {
+                                    print("Error downloading pokemon picture: \(e)")
+                                } else {
+                                    // No errors found.
+                                    // It would be weird if we didn't have a response, so check for that too.
+                                    if (response as? HTTPURLResponse) != nil {
+                                        if let imageData = data {
+                                            // Finally convert that Data into an image and do what you wish with it.
+                                            let onlineImage = UIImage(data: imageData)
+                                            // Do something with your image.
+                                            
+                                            self.pokemonImages.append(onlineImage!)
+                                            
+                                        } else {
+                                            print("Couldn't get image: Image is nil")
+                                        }
+                                    } else {
+                                        print("Couldn't get response code for some reason")
+                                    }
+                                }
+                            }
+                            
+                            downloadPicTask.resume()*/
+                            
+                        } //else { break }
                     
-                    //return pokedex as [String : Any]
+                    }
                     
                 } else if let object = json as? [String: Any] {
                     // json is an array
@@ -99,41 +124,8 @@ class PokedexCollectionViewController: UICollectionViewController {
             }
         } catch {
             print(error.localizedDescription)
-        }
-        
+        }//end of catch
    
-        //let returnDefault: [String: Any] = ("" as? [String: Any])!
-        
-        //return returnDefault
-    }
+    }//end of func readPokemonJson
     
-    
-    
-    //func readJSONObject(object: [String: AnyObject]) -> [String : AnyObject] {
-        //guard let pokemonID = object["id"] as? String,
-            //let version = object["swiftVersion"] as? Float,
-            //let pocketMonsters = object["pokemon"] as? [[String: AnyObject]] else { return }
-        
-        //for pokemon in pocketMonsters {
-            //guard let num = pokemon["num"] as? String,
-                //let name = pokemon["name"] as? String else { break }
-            
-                //print(num + name)
-            
-            
-            //switch num {
-            //case "001":
-                //print(num + " is \(name) years old.")
-            //case "002":
-               // _ = num + " is \(name) years old."
-            //case "003":
-               // _ = num + " is \(name) years old."
-            //default:
-                //break
-            //}
-        //}
-    //}
-    
-    
-    
-}
+}//end of class
